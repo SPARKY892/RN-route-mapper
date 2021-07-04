4aa9e48afbe25e2e7832e2e9298ce447b231498a
Mark <marksmellie892@gmail.com>
2021-07-04 21:12:03 +0100

    Updating Polyline props to reoslve error

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
 	//This needs to change every time ngrok is run expires in 2 hours
-	baseURL: "http://214bc4d37c84.ngrok.io",
+	baseURL: "http://65ba88cd0752.ngrok.io",
 });
