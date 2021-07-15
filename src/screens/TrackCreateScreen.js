fb7d3534d889c64105c4f04edf0e20668e2860cc
Mark <marksmellie892@gmail.com>
2021-07-15 21:01:54 +0100

    Removed redundant code

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,22 @@
-import React from "react";
+import "../_mockLocation";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Map from "../components/Map";
+import { Context as LocationContext } from "../context/LocationContext";
+import useLocation from "../hooks/useLocation";
 
 const TrackCreateScreen = () => {
+	const { addLocation } = useContext(LocationContext);
+
+	const [err] = useLocation((location) => addLocation(location));
+
 	return (
 		<SafeAreaView forceInset={{ top: "always" }}>
 			<Text h2>Create a Track</Text>
 			<Map />
+			{err ? <Text>Please enable location services</Text> : null}
 		</SafeAreaView>
 	);
 };
