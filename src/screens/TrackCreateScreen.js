0a8d7cbfdf21af5c0b4bfc0a2f8608478a490dd5
Mark <marksmellie892@gmail.com>
2021-07-16 20:36:02 +0100

    Added LocationContext cases for start,stop,add

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,24 +3,28 @@ import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { withNavigationFocus } from "react-navigation";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
+import TrackForm from "../components/TrackForm";
 
-const TrackCreateScreen = () => {
-	const { addLocation } = useContext(LocationContext);
-
-	const [err] = useLocation((location) => addLocation(location));
+const TrackCreateScreen = ({ isFocused }) => {
+	const { state, addLocation } = useContext(LocationContext);
+	const [err] = useLocation(isFocused, (location) => {
+		addLocation(location, state.recording);
+	});
 
 	return (
 		<SafeAreaView forceInset={{ top: "always" }}>
 			<Text h2>Create a Track</Text>
 			<Map />
 			{err ? <Text>Please enable location services</Text> : null}
+			<TrackForm />
 		</SafeAreaView>
 	);
 };
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
