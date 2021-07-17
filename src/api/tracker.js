7f5edb7f4350ef7630b022ef06456e5aa509ac95
Mark <marksmellie892@gmail.com>
2021-07-17 13:52:53 +0100

    Added auth to all api calls by defualt

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,22 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
+const instance = axios.create({
 	//This needs to change every time ngrok is run expires in 2 hours
-	baseURL: "http://65ba88cd0752.ngrok.io",
+	baseURL: "http://1c11e6b72d1e.ngrok.io",
 });
+
+instance.interceptors.request.use(
+	async (config) => {
+		const token = await AsyncStorage.getItem("token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(err) => {
+		return Promise.reject(err);
+	}
+);
+
+export default instance;
